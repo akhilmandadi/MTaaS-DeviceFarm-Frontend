a4f44ea4b8cac328602c84edaeb88c9e8f35bd98
Akhil Reddy <akhilreddymandadi@gmail.com>
2020-05-11 13:42:51 -0700

    Profile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,6 +23,7 @@ class NavBar extends Component {
             navBar = (
                 <ul class="nav navbar-nav navbar-right">
                     <li><Link to="/tester/projects" style={{ color: "white" }}><span class="glyphicon glyphicon-"></span><b>Projects</b></Link></li>
+                    <li><Link to={"/tester/"+sessionStorage.getItem("id")+"/profile"} style={{ color: "white" }}><span class="glyphicon glyphicon-"></span><b>Profile</b></Link></li>
                     <li><Link to="/signin" onClick={this.handleLogout} style={{ color: "white" }}><span class="glyphicon glyphicon-log-out"></span> <b>Logout</b></Link></li>
                 </ul>
             )
